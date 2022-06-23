aeaf53bf3fce873520e65da20fe2b601d6db05f8
shrigharat <gharatshri014@gmail.com>
2022-06-23 15:29:38 +0530

    Initial review commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ThemeService } from './services/theme.service';
 
+
+//Review branch
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
